f56d4a8d9993e70a2e4c4740202edbeec44a9e83
sofia <sofiraspanti@gmail.com>
2022-06-22 19:15:43 -0300

    commit inicial

---
+++ metallurgy/src/main/java/projectcompany/metallurgy/MetallurgyApplication.java
@@ -0,0 +1,13 @@
+package projectcompany.metallurgy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetallurgyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetallurgyApplication.class, args);
+	}
+
+}
---
+++ metallurgy/src/test/java/projectcompany/metallurgy/MetallurgyApplicationTests.java
@@ -0,0 +1,13 @@
+package projectcompany.metallurgy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetallurgyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
